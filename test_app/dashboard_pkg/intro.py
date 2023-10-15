3ea10a4d28777fec53198f8dc94ce18f3ca939e4
Pia <piatorain@yahoo.com>
2023-10-15 00:10:10 -0700

    play and stats

---
+++ test_app/dashboard_pkg/intro.py
@@ -0,0 +1,46 @@
+import fontstyle
+
+def show_intro():
+
+    text = fontstyle.apply('|                Welcome to Trivia Game                  |', 'bold/italic/cyan')
+    
+    border_side = fontstyle.apply('|                                                        |', 'bold/italic/cyan')
+    border_horizontal = fontstyle.apply('------------------------------------------------------', 'bold/italic/cyan')
+    mid_horizontal = fontstyle.apply('|    🌟-------------------------------------------🌟     |', 'bold/italic/cyan')
+    dash_options_1 = fontstyle.apply('|     | 1. Instructions      |     2. Play Game    |     |', 'bold/italic/cyan')
+    dash_options_2 = fontstyle.apply('|     | 3. Stats             |     4. Exit         |     |', 'bold/italic/cyan')
+    print("")
+
+
+    print(f"🌟{border_horizontal}⭐")
+    print(border_side)
+    print(text) 
+    print(border_side)                    
+    print(border_side)                    
+    print(border_side)  
+    print(mid_horizontal) 
+    print(dash_options_1)  
+    print(mid_horizontal)               
+    print(dash_options_2)   
+    print(mid_horizontal)   
+    print(border_side)                    
+    print(border_side) 
+    print(f"🌟{border_horizontal}⭐")
+
+
+
+def show_difficulty():
+    difficulty_text = fontstyle.apply('       ===  Difficulty Level ===           ', 'bold/italic/yellow')
+    difficulty_horizontal = fontstyle.apply('----------------------------------------', 'bold/italic/yellow')
+    difficulty_selections = fontstyle.apply('|   1. Easy       |   2. Average       |   ', 'bold/italic/yellow')
+    difficulty_selections_2 = fontstyle.apply('|   3. Hard       |   4. Home          |', 'bold/italic/yellow')
+
+    print("")   
+    print(f"{difficulty_text}")
+    print(difficulty_horizontal)
+    print(difficulty_selections)
+    print(difficulty_horizontal)
+    print(difficulty_selections_2)
+    print(difficulty_horizontal)
+    
+    
